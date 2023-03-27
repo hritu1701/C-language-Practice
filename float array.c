da1255f8942f3ea8b7d1d03100137951378990b5
HRITU RAJ <112776533+sac3xhin@users.noreply.github.com>
2023-03-27 09:49:03 +0530

    Add files via upload

---
+++ float array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float marks[3]={90.5,92.5,96.5};
+int a=0;
+while (a<3)
+{
+printf("%.2f,",marks[a]);
+a++;
+}
+return 0;
+}
---
+++ pre increment.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a[5]={5,1,15,20,25};
+int i=2;
+printf("%d",a[++i]);
+return 0;
+}
